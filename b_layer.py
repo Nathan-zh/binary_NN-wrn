61a23c89933ce5437181e4375ecb9f52ed65cbb8
Qiang <zhangqiang@tamu.edu>
2019-03-28 15:19:16 -0500

    4

---
+++ b_layer.py
@@ -0,0 +1,139 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import copy
+import sys
+import types as python_types
+import warnings
+
+import numpy as np
+
+from tensorflow.python.eager import context
+from tensorflow.python.framework import common_shapes
+from tensorflow.python.framework import ops
+from tensorflow.python.framework import tensor_shape
+from tensorflow.python.keras import activations
+from tensorflow.python.keras import backend as K
+from tensorflow.python.keras import constraints
+from tensorflow.python.keras import initializers
+from tensorflow.python.keras import regularizers
+from tensorflow.python.keras.engine.base_layer import Layer
+from tensorflow.python.keras.engine.input_spec import InputSpec
+from tensorflow.python.keras.utils import conv_utils
+from tensorflow.python.keras.utils import generic_utils
+from tensorflow.python.keras.utils import tf_utils
+from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import gen_math_ops
+from tensorflow.python.ops import math_ops
+from tensorflow.python.ops import nn
+from tensorflow.python.ops import nn_ops
+from tensorflow.python.ops import standard_ops
+from tensorflow.python.util.tf_export import tf_export
+
+
+class Dense(Layer):
+
+  def __init__(self,
+               units,
+               activation=None,
+               use_bias=True,
+               kernel_initializer='glorot_uniform',
+               bias_initializer='zeros',
+               kernel_regularizer=None,
+               bias_regularizer=None,
+               activity_regularizer=None,
+               kernel_constraint=None,
+               bias_constraint=None,
+               **kwargs):
+    if 'input_shape' not in kwargs and 'input_dim' in kwargs:
+      kwargs['input_shape'] = (kwargs.pop('input_dim'),)
+
+    super(Dense, self).__init__(
+        activity_regularizer=regularizers.get(activity_regularizer), **kwargs)
+    self.units = int(units)
+    self.activation = activations.get(activation)
+    self.use_bias = use_bias
+    self.kernel_initializer = initializers.get(kernel_initializer)
+    self.bias_initializer = initializers.get(bias_initializer)
+    self.kernel_regularizer = regularizers.get(kernel_regularizer)
+    self.bias_regularizer = regularizers.get(bias_regularizer)
+    self.kernel_constraint = constraints.get(kernel_constraint)
+    self.bias_constraint = constraints.get(bias_constraint)
+
+    self.supports_masking = True
+    self.input_spec = InputSpec(min_ndim=2)
+
+  def build(self, input_shape):
+    input_shape = tensor_shape.TensorShape(input_shape)
+    if tensor_shape.dimension_value(input_shape[-1]) is None:
+      raise ValueError('The last dimension of the inputs to `Dense` '
+                       'should be defined. Found `None`.')
+    last_dim = tensor_shape.dimension_value(input_shape[-1])
+    self.input_spec = InputSpec(min_ndim=2,
+                                axes={-1: last_dim})
+    self.kernel = self.add_weight(
+        'kernel',
+        shape=[last_dim, self.units],
+        initializer=self.kernel_initializer,
+        regularizer=self.kernel_regularizer,
+        constraint=self.kernel_constraint,
+        dtype=self.dtype,
+        trainable=True)
+    if self.use_bias:
+      self.bias = self.add_weight(
+          'bias',
+          shape=[self.units,],
+          initializer=self.bias_initializer,
+          regularizer=self.bias_regularizer,
+          constraint=self.bias_constraint,
+          dtype=self.dtype,
+          trainable=True)
+    else:
+      self.bias = None
+    self.built = True
+
+  def call(self, inputs):
+    inputs = ops.convert_to_tensor(inputs)
+    rank = common_shapes.rank(inputs)
+    if rank > 2:
+      # Broadcasting is required for the inputs.
+      outputs = standard_ops.tensordot(inputs, self.kernel, [[rank - 1], [0]])
+      # Reshape the output back to the original ndim of the input.
+      if not context.executing_eagerly():
+        shape = inputs.get_shape().as_list()
+        output_shape = shape[:-1] + [self.units]
+        outputs.set_shape(output_shape)
+    else:
+      outputs = gen_math_ops.mat_mul(inputs, self.kernel)
+    if self.use_bias:
+      outputs = nn.bias_add(outputs, self.bias)
+    if self.activation is not None:
+      return self.activation(outputs)  # pylint: disable=not-callable
+    return outputs
+
+  def compute_output_shape(self, input_shape):
+    input_shape = tensor_shape.TensorShape(input_shape)
+    input_shape = input_shape.with_rank_at_least(2)
+    if tensor_shape.dimension_value(input_shape[-1]) is None:
+      raise ValueError(
+          'The innermost dimension of input_shape must be defined, but saw: %s'
+          % input_shape)
+    return input_shape[:-1].concatenate(self.units)
+
+  def get_config(self):
+    config = {
+        'units': self.units,
+        'activation': activations.serialize(self.activation),
+        'use_bias': self.use_bias,
+        'kernel_initializer': initializers.serialize(self.kernel_initializer),
+        'bias_initializer': initializers.serialize(self.bias_initializer),
+        'kernel_regularizer': regularizers.serialize(self.kernel_regularizer),
+        'bias_regularizer': regularizers.serialize(self.bias_regularizer),
+        'activity_regularizer':
+            regularizers.serialize(self.activity_regularizer),
+        'kernel_constraint': constraints.serialize(self.kernel_constraint),
+        'bias_constraint': constraints.serialize(self.bias_constraint)
+    }
+    base_config = super(Dense, self).get_config()
+    return dict(list(base_config.items()) + list(config.items()))
--- binaryNN.py
+++ binaryNN.py
@@ -1,21 +1,8 @@
 from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
 import numpy as np
-#from Binary_Dense import Binary_Dense
-
-
-def binarize(aa2):
-    aa1 = []
-    for i in range(len(aa2)):
-        if i % 2 == 0:
-            aa1.append(np.sign(aa2[i]))
-    return aa1
-
-
-def clip(bb1):
-    bb1[bb1 > 1] = 1
-    bb1[bb1 < -1] = -1
-
+import time
+import b_layer as binary_layer
 
 # load dataset and preprocess
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
@@ -37,23 +24,23 @@ tf.reset_default_graph()
 inputs = tf.placeholder(tf.float32, [None, 28, 28, 1], name='input')
 outputs = tf.placeholder(tf.float32, [None, 10], name='output')
 
-x1 = tf.layers.conv2d(inputs=inputs,
-                     filters=4,
-                     kernel_size=(7, 7),
-                     strides=(3, 3))
-x2 = tf.nn.tanh(x1)
-x = tf.transpose(x2, perm=[0, 3, 1, 2])
+x = binary_layer(inputs=inputs,
+                 filters=4,
+                 kernel_size=(7, 7),
+                 strides=(3, 3))
+
+x = tf.transpose(x, perm=[0, 3, 1, 2])
 x = tf.layers.flatten(x)
-x3 = tf.layers.dense(x, units=64)
-x4 = tf.nn.tanh(x3)
-pred = tf.layers.dense(x4, units=10)
+x = binary_layer(x, units=64)
+pred = binary_layer(x, units=10)
 
 loss = tf.losses.softmax_cross_entropy(outputs, pred)
 tf.summary.scalar('loss', loss)
 
-train = tf.train.GradientDescentOptimizer(learning_rate=1e-4)
+l_r = 1e-3
+train = tf.train.AdamOptimizer(learning_rate=l_r)
 grads = train.compute_gradients(loss, tf.trainable_variables())
-#train1 = train.apply_gradients(grads)
+train1 = train.apply_gradients(grads)
 
 correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(outputs, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
@@ -65,55 +52,23 @@ val_writer = tf.summary.FileWriter('./logdir/val', accuracy.graph)
 saver = tf.train.Saver()
 
 # set hyper-parameters
-batch_size = 1000
+batch_size = 100
 epochs = 500
+old_acc = 0
 
-# training
+# training and validation
 with tf.Session() as sess:
     print('*****************Training Start!*****************')
     sess.run(tf.global_variables_initializer())
     for m in range(epochs):
         iterations = int(mnist.train.num_examples/batch_size)
         for i in range(iterations):
-            vars_vals = sess.run(tf.trainable_variables())
-            #print(sess.run(tf.trainable_variables()[0][:, :, 0, 0]))
-            #print('**********************************************************')
-
-            # binarize weights and assign
-            b_weights = binarize(vars_vals)
-            sess.run(tf.assign(tf.trainable_variables()[0], b_weights[0]))
-            sess.run(tf.assign(tf.trainable_variables()[2], b_weights[1]))
-            sess.run(tf.assign(tf.trainable_variables()[4], b_weights[2]))
 
             batch_x, batch_y = mnist.train.next_batch(batch_size)
             batch_x = np.reshape(batch_x, [batch_size, 28, 28, 1])
-            gradients, loss_train, prediction, summary, xx1, xx2, xx3, xx4 = \
-                sess.run([grads, loss, pred, merged, x1, x2, x3, x4], {inputs: batch_x, outputs: batch_y})
-            #print(- gradients[0][0][:, :, 0, 0] * 1e-4 + gradients[0][1][:, :, 0, 0])
-            #print('**********************************************************')
-
-            for ii in range(len(tf.trainable_variables())):
-                new_weights = - gradients[ii][0] * 1e-4 + vars_vals[ii]
-                if ii % 2 == 0:
-                    clip(new_weights)
-                sess.run(tf.assign(tf.trainable_variables()[ii], new_weights))
-            '''
-            sess.run(tf.assign(grads[0][1], vars_vals[0]))
-            sess.run(tf.assign(grads[2][1], vars_vals[2]))
-            sess.run(tf.assign(grads[4][1], vars_vals[4]))
-            '''
-            #sess.run(train1, {inputs: batch_x, outputs: batch_y})
-
-            #print(sess.run(tf.trainable_variables()[0][:, :, 0, 0]))
-            #print('**********************************************************')
+            _, loss_train, summary = sess.run([train1, loss, merged], {inputs: batch_x, outputs: batch_y})
             train_writer.add_summary(summary, i+m*iterations)
 
-        #print('prediction is : ', prediction[0, :])
-        vars_vals = sess.run(tf.trainable_variables())
-        b_weights = binarize(vars_vals)
-        sess.run(tf.assign(tf.trainable_variables()[0], b_weights[0]))
-        sess.run(tf.assign(tf.trainable_variables()[2], b_weights[1]))
-        sess.run(tf.assign(tf.trainable_variables()[4], b_weights[2]))
         val_x = np.reshape(mnist.validation.images, [5000, 28, 28, 1])
         val_y = mnist.validation.labels
         val_acc, summary = sess.run([accuracy, merged], {inputs: val_x, outputs: val_y})
@@ -121,14 +76,9 @@ with tf.Session() as sess:
         print('Epoch: {}'.format(m + 1),
               'Train_loss: {:.3f}'.format(loss_train),
               'Val_accuracy: {:.3f}'.format(val_acc))
-        sess.run(tf.assign(tf.trainable_variables()[0], vars_vals[0]))
-        sess.run(tf.assign(tf.trainable_variables()[2], vars_vals[2]))
-        sess.run(tf.assign(tf.trainable_variables()[4], vars_vals[4]))
-        '''
-        for iii in range(len(tf.trainable_variables())):
-            assert vars_vals[iii].all() <= 1 and vars_vals[iii].all() >= -1
-        '''
-    saver.save(sess, './model/final.ckpt')
+
+        if val_acc > old_acc:
+            saver.save(sess, './model/final.ckpt')
 
 print('*****************Training End!*****************')
 train_writer.close()
